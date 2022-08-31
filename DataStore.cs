48fc46f131bc2e2411711948cc416e8b6086f42a
ricardotlpz <ricardo-tejada@live.com.mx>
2022-08-30 19:54:28 -0500

    init

---
+++ DataStore.cs
@@ -0,0 +1,8 @@
+namespace DataStore
+{
+    public class DataArray
+    {
+        string[] array = new string[6];
+        //array[0] = Employee.ID;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿public class Employee
+{
+    public int ID {get;set;}
+    public string Name {get;set;}
+    public string LastName {get;set;}
+    public string RFC {get;set;}
+    public DateTime BornDate {get;set;}
+    public EmployeeStatus Status {get;set;}    
+
+    public enum EmployeeStatus
+    {
+        NotSet,
+        Active,
+        Inactive
+    }
+    static void Main(string[] args)
+    {
+        // Say hello!
+        Console.WriteLine("Hello there!");
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("1= Add employees to list");
+
+        var action = Console.ReadLine();
+
+        switch(action)
+        {
+            case "1":
+            Fetch.FetchData();
+            break;
+        }
+
+    }
+}
